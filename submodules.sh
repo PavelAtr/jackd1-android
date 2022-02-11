dee8a1b75765803db208bebc9b33dbe00e72ff63
Pavel <pvsamsonov76@gmail.com>
2022-02-11 18:13:30 +0300

    Update submodule android-shm.

--- submodules.sh
+++ submodules.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-git submodule init android-shm
-git submodule init jackd2-opensles
-git submodule init jack2
-
-git submodule update android-shm
-git submodule update jackd2-opensles
-git submodule update jack2
+git submodule sync --recursive
+git submodule update --init --recursive
